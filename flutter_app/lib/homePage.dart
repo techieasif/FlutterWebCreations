27647badd2d6c804a680293f46b193176ec56b2f
Asif <techieasif@gmail.com>
2020-10-02 10:41:26 +0530

    tests result good

--- flutter_app/lib/homePage.dart
+++ flutter_app/lib/homePage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -24,48 +26,71 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         key: scfKey,
         backgroundColor: Colors.transparent,
-        body: Stack(
+        body: Column(
           children: [
-            Positioned(
-                top: 0.0,
-                right: 0.0,
-                left: 0.0,
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.yellow,
-                  ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.yellow,
+              ),
 
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                          icon: Icon(CupertinoIcons.home, color: Colors.white),
-                          onPressed: () {
-                            scfKey.currentState
-                              ..hideCurrentSnackBar()
-                              ..showSnackBar(SnackBar(content: Text("Woww")));
-                          }),
-                      IconButton(
-                          icon: Icon(CupertinoIcons.person_alt, color: Colors.white),
-                          onPressed: () {
-                            scfKey.currentState
-                              ..hideCurrentSnackBar()
-                              ..showSnackBar(SnackBar(content: Text("Woww")));
-                          }),
-                      IconButton(
-                          icon: Icon(CupertinoIcons.cloud_moon_rain, color: Colors.white,),
-                          onPressed: () {
-                            scfKey.currentState
-                              ..hideCurrentSnackBar()
-                              ..showSnackBar(SnackBar(content: Text("Woww")));
-                          }),
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                      icon: Icon(CupertinoIcons.home, color: Colors.white),
+                      onPressed: () {
+                        scfKey.currentState
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(SnackBar(content: Text("Woww")));
+                      }),
+                  IconButton(
+                      icon: Icon(CupertinoIcons.person_alt, color: Colors.white),
+                      onPressed: () {
+                        scfKey.currentState
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(SnackBar(content: Text("Woww")));
+                      }),
+                  IconButton(
+                      icon: Icon(CupertinoIcons.cloud_moon_rain, color: Colors.white,),
+                      onPressed: () {
+                        scfKey.currentState
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(SnackBar(content: Text("Woww")));
+                      }),
 
-                    ],
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 10250,
+                  itemBuilder: (context, i){
+                    if(i % 7 == 0 ){
+                      return Container(
+                        width: double.infinity,
+                        height: 400,
+                        child: Image.network("https://images.unsplash.com/photo-1486649567693-aaa9b2e59385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"),
+                      );
+                    }
+                    if(i % 11 == 0){
+                      return Container(
+                        width: double.infinity,
+                        height: 400,
+                        child: Image.network("https://images.unsplash.com/photo-1527189919029-aeb3d997547d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80"),
+                      );
+                    }
+                return Card(
+                  color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text("$i", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
                   ),
-                ))
+                );
+              }),
+            )
           ],
         ),
       ),
